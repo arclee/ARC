bfa41beb28a0b1d5de15f1eb30f70c3a3f4596b7
arclee <arclee0117@gmail.com>
2014-03-17 23:03:35 +0800

    create

---
+++ Assets/ARCRoot/ARC/Code/GameObject/arcBaseGameObject.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class arcBaseGameObject : MonoBehaviour {
+
+	bool _enable = true;
+	//自己的方法.
+	void _Update()
+	{
+		if (!_enable)
+		{
+			return;
+		}
+
+	}
+
+}
---
+++ Assets/ARCRoot/ARC/Editor/arcGameObjEditor.cs
@@ -0,0 +1,27 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System.Collections;
+
+public class arcGameObjEditor : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+		
+	[MenuItem("GameObject/Create Other/ARC/GameObject", false, 13000)]
+	static void DoCreateSpriteObject()
+	{
+		
+		GameObject go = new GameObject("arcGameObject");
+		arcGameObject script = go.AddComponent<arcGameObject>();
+		Selection.activeGameObject = go;
+		Undo.RegisterCreatedObjectUndo(go, "Create arcGameObject");
+
+	}
+}

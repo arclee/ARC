81df5ea7fc63adca28c53ebeaf263135191b2023
arclee <arclee0117@gmail.com>
2014-03-28 14:09:45 +0800

    加入 JiggleBone (可簡易乳搖)

--- Assets/ARCRoot/ARC/Code/Utility/arcUtility.cs
+++ Assets/ARCRoot/ARC/Code/Utility/arcUtility.cs
@@ -37,4 +37,44 @@ public static class arcUtility
 			return (T)formatter.Deserialize(stream);
 		}
 	}
+
+	
+	//由 X 當 forward 的 lookat.
+	static public Matrix4x4 LookAtFromX(Vector3 eye, Vector3 target, Vector3 up)
+	{
+		//http://games.greggman.com/game/webgl-3d-cameras/
+		//http://3dgep.com/?p=1700#The_View_Matrix
+		//http://msdn.microsoft.com/en-us/library/windows/desktop/bb281710(v=vs.85).aspx
+
+		Vector3 vx = eye - target; // The "forward".
+		vx.Normalize();
+		Vector3 vz = Vector3.Cross(up, vx);// The "right" .
+		// vy doesn't need to be normalized because it's a cross
+		// product of 2 normalized vectors
+		Vector3 vy = Vector3.Cross(vz, vx);// The "up".
+		Matrix4x4 inverseViewMatrix = new Matrix4x4();
+		
+		inverseViewMatrix.SetColumn(0, new Vector4(vx.x, vx.y, vx.z , 0));//
+		inverseViewMatrix.SetColumn(1, new Vector4(vy.x, vy.y, vy.z , 0));//
+		inverseViewMatrix.SetColumn(2, new Vector4(vz.x, vz.y, vz.z , 0));//用 right 當 forward.
+		//inverseViewMatrix.SetColumn(3, new Vector4(-Vector3.Dot(vx, eye), -Vector3.Dot(vy, eye), -Vector3.Dot(vz, eye) , 1));
+		inverseViewMatrix.SetColumn(3, new Vector4(eye.x, eye.y, eye.z , 1));
+		
+		return inverseViewMatrix.inverse;
+	}
+
+	//由 matrix 中 取出 quaternion.
+	static public Quaternion QuaternionFromMatrix(Matrix4x4 m)
+	{
+		// Adapted from: http://www.euclideanspace.com/maths/geometry/rotations/conversions/matrixToQuaternion/index.htm
+		Quaternion q = new Quaternion();
+		q.w = Mathf.Sqrt( Mathf.Max( 0, 1 + m[0,0] + m[1,1] + m[2,2] ) ) / 2; 
+		q.x = Mathf.Sqrt( Mathf.Max( 0, 1 + m[0,0] - m[1,1] - m[2,2] ) ) / 2; 
+		q.y = Mathf.Sqrt( Mathf.Max( 0, 1 - m[0,0] + m[1,1] - m[2,2] ) ) / 2; 
+		q.z = Mathf.Sqrt( Mathf.Max( 0, 1 - m[0,0] - m[1,1] + m[2,2] ) ) / 2; 
+		q.x *= Mathf.Sign( q.x * ( m[2,1] - m[1,2] ) );
+		q.y *= Mathf.Sign( q.y * ( m[0,2] - m[2,0] ) );
+		q.z *= Mathf.Sign( q.z * ( m[1,0] - m[0,1] ) );
+		return q;
+	}
 }

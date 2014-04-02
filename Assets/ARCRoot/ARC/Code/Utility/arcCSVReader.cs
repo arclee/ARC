551e95b90c473ca937dfeda3102e41e34d23e69b
arclee <arclee0117@gmail.com>
2014-04-02 17:57:26 +0800

    加入 csv 的 reader (不太完美) 加入測試 csv 用的 resources 資料夾

---
+++ Assets/ARCRoot/ARC/Code/Utility/arcCSVReader.cs
@@ -0,0 +1,107 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq; 
+
+public class arcCSVReader
+{
+
+	string mFileName = "";
+
+	TextAsset mTextAsset = null;
+
+	List<string[]> mData = new List<string[]>();
+
+	public bool LoadFromResource(string filename)
+	{
+		mFileName = filename;
+		mTextAsset = Resources.Load(mFileName) as TextAsset;
+
+		if (mTextAsset == null)
+		{
+			return false;
+		}
+
+		//mData = SplitCsvGrid(mTextAsset.text);
+		Parse();
+		return true;
+
+	}
+
+	void Parse()
+	{
+		//取出每一行.
+		string [] lineArray = mTextAsset.text.Split(new string[] {"\n", "\r\n"}, System.StringSplitOptions.RemoveEmptyEntries);
+
+		//存放.
+
+
+
+		for(int i =0;i < lineArray.Length; i++)
+		{
+			//略過.
+			if (lineArray[i].Length > 0)
+			{
+				if (lineArray[i][0] == '#' || lineArray[i][0] == '\r' || lineArray[i][0] == '\n')
+				{
+				    continue;
+				}
+			}
+			
+			if (lineArray[i].Length > 1)
+			{
+				if ((lineArray[i][0] == '\\' && lineArray[i][1] == '\\') || (lineArray[i][0]=='/' && lineArray[i][1]=='/'))
+				{
+					continue;
+				}
+			}
+
+			mData.Add(SplitCsvLine(lineArray[i]));
+		}
+
+	}
+
+	// splits a CSV row 
+	static public string[] SplitCsvLine(string line)
+	{
+		string pattern = @"
+		    # Match one value in valid CSV string.
+		    (?!\s*$)                                      # Don't match empty last value.
+		    \s*                                           # Strip whitespace before value.
+		    (?:                                           # Group for value alternatives.
+		      '(?<val>[^'\\]*(?:\\[\S\s][^'\\]*)*)'       # Either $1: Single quoted string,
+		    | ""(?<val>[^""\\]*(?:\\[\S\s][^""\\]*)*)""   # or $2: Double quoted string,
+		    | (?<val>[^,'""\s]*(?:\s+[^,'""\s]+)*)    		# or $3: Non-comma, non-quote stuff.
+		    )                                             # End group of value alternatives.
+		    \s*                                           # Strip whitespace after value.
+		    (?:,|$)                                       # Field ends on comma or EOS.
+		    ";
+
+		//[^,'""\s] 非 , ' " \s 的字都選起來
+
+		string[] values = (from System.Text.RegularExpressions.Match m in System.Text.RegularExpressions.Regex.Matches(
+							line
+							, pattern
+			, System.Text.RegularExpressions.RegexOptions.ExplicitCapture | System.Text.RegularExpressions.RegexOptions.IgnorePatternWhitespace | System.Text.RegularExpressions.RegexOptions.Multiline)
+		                   select m.Groups[1].Value).ToArray();
+
+		return values;
+	}
+
+	
+	// outputs the content of a 2D array, useful for checking the importer
+	public void DebugOutput()
+	{		
+		string textOutput = ""; 
+		for(int i =0;i < mData.Count; i++)
+		{
+			for (int j = 0; j < mData[i].Length; j++)
+			{
+				textOutput += mData[i][j];
+				textOutput += ","; 
+			}
+			textOutput += "\n"; 
+		}
+		Debug.Log(textOutput);
+	}
+}
--- Assets/ARCRoot/Demo/test.cs
+++ Assets/ARCRoot/Demo/test.cs
@@ -1,27 +1,35 @@
 ﻿using UnityEngine;
 using System.Collections;
 public class test : MonoBehaviour {
-
+	
+	arcCSVReader cr = new arcCSVReader();
 	// Use this for initialization
 	void Start () {
 		
-		Rigidbody2D ant = gameObject.GetSafeComponent<Rigidbody2D>();
-		Animator aaa =gameObject.GetInterfaceComponent<Animator>();
-		Vector3 v3 = new Vector3(1,2,3);
+		//Rigidbody2D ant = gameObject.GetSafeComponent<Rigidbody2D>();
+		//Animator aaa =gameObject.GetInterfaceComponent<Animator>();
+		//Vector3 v3 = new Vector3(1,2,3);
 		
-		Vector3 cv3 = arcUtility.Clone(v3);
-		Vector3 cv43 = arcUtility.Clone(cv3);
+		//Vector3 cv3 = arcUtility.Clone(v3);
+		//Vector3 cv43 = arcUtility.Clone(cv3);
 
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
-		Rigidbody2D ant = gameObject.GetSafeComponent<Rigidbody2D>();
-		Animator aaa =gameObject.GetInterfaceComponent<Animator>();
-		Vector3 v3 = new Vector3(1,2,3);
-		
-		Vector3 cv3 = arcUtility.Clone(v3);
-		Vector3 cv43 = arcUtility.Clone(cv3);
+
 	}
+
+	void OnGUI()
+	{
+		if (GUI.Button(new Rect(0,0,100,100), "load csv"))
+		{
+			cr.LoadFromResource("story");
+
+			cr.DebugOutput();
+		}
+
+	}
+
 }

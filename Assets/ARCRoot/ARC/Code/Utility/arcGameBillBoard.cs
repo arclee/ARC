f7326d600d144c0f1a6f0fb50badf8b352e62116
arclee <arclee0117@gmail.com>
2015-03-10 17:26:14 +0800

    加入利用 transform 做 billboard.

---
+++ Assets/ARCRoot/ARC/Code/Utility/arcGameBillBoard.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class arcGameBillBoard : MonoBehaviour {
+
+	public enum LookMode
+	{
+		LookCam = 0,
+		LookCamFoward
+	}
+
+	public LookMode lookmode = LookMode.LookCam;
+	public bool keepYup = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (lookmode == LookMode.LookCam)
+		{
+			Vector3 cam = Camera.main.transform.position;
+			if (keepYup)
+			{
+				cam.y = transform.position.y;
+			}
+			transform.LookAt(transform.position - cam + transform.position);
+		}
+		else if (lookmode == LookMode.LookCamFoward)
+		{
+			Vector3 cam = Camera.main.transform.forward;
+			if (keepYup)
+			{
+				cam.y = 0;
+				cam.Normalize();
+			}
+			
+			transform.LookAt(transform.position + cam);
+		}
+
+	}
+}

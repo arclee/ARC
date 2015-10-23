af0b88d211a14a6e56b3f6655d707be140e24635
Arc <arclee@users.noreply.github.com>
2015-10-24 02:59:26 +0800

    物件管理器

---
+++ Assets/ARCRoot/ARC/Code/Utility/arcGameObjPool.cs
@@ -0,0 +1,115 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameObjPool
+{
+
+	public string prefabName = "";
+	public GameObject templatePrefabObj;
+	public bool grawable = true;
+	public int maxobj = 10;	
+	public List<GameObject> pool = new List<GameObject>();
+	int poolidx = 0;
+	
+	GameObject gameObject;
+	//GameObject parentGameObject;
+	// Use this for initialization
+
+	void Inital()
+	{
+		if (gameObject == null)
+		{
+			gameObject = new GameObject(prefabName);
+		}
+	}
+
+	public void PreCreate (GameObject parent)
+	{
+		Inital();
+		gameObject.transform.parent = parent.transform;
+		templatePrefabObj = (GameObject)Resources.Load(prefabName);
+		if (!templatePrefabObj)
+		{
+			//Debug.Log(this.GetType().ToString() + " PreCreate fail " + prefabName);
+			return;
+		}
+
+	
+		for (int i = 0; i < maxobj; i++)
+		{
+			CreateOneObj();
+		}
+	}
+	
+	public void PreCreatePrefab (GameObject prefab, GameObject parent)
+	{
+		Inital();
+		gameObject.transform.parent = parent.transform;
+		templatePrefabObj = prefab;
+		if (!templatePrefabObj)
+		{
+			Debug.Log(this.GetType().ToString() + " PreCreate fail " + prefabName);
+			return;
+		}
+		
+		
+		for (int i = 0; i < maxobj; i++)
+		{
+			CreateOneObj();
+		}
+	}
+
+	GameObject CreateOneObj()
+	{
+		GameObject newob = (GameObject)GameObject.Instantiate(templatePrefabObj);
+		
+		newob.transform.parent = gameObject.transform;
+
+		newob.name = newob.name + pool.Count.ToString();
+		newob.SetActive(false);
+		pool.Add(newob);
+		return newob;
+	}
+
+	public void Restore(GameObject obj)
+	{
+//		if (pool.Contains(obj))
+//		{
+//			obj.SetActive(false);
+//		}
+	}
+
+	public GameObject GetObject()
+	{
+		int poolcount = pool.Count;
+		for (int i = 0; i < maxobj; i++)
+		{
+			poolidx = (poolidx + 1) % poolcount;
+			if (!pool[poolidx].activeSelf)
+			{
+				pool[poolidx].SetActive(true);
+				return pool[poolidx];
+			}			
+		}
+
+		if (grawable)
+		{
+			GameObject newob = CreateOneObj();
+			newob.SetActive(true);
+			return newob;
+		}
+
+		return null;
+	}
+
+	public void DisableAll()
+	{
+		int poolcount = pool.Count;
+		for (int i = 0; i < poolcount; i++)
+		{
+			pool[i].SetActive(false);		
+		}
+
+	}
+}
---
+++ Assets/ARCRoot/ARC/Code/Utility/arcGamePoolMgr.cs
@@ -0,0 +1,115 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class arcGamePoolMgr<T> : MonoBehaviour where T : MonoBehaviour
+{
+	
+	static  public arcGamePoolMgr<T> Instance;
+
+	public bool Grawable = false;
+	public int MaxObj = 10;
+	public int debugid = 0;
+	
+		
+	protected Dictionary<string, GameObjPool> pooldict = new Dictionary<string, GameObjPool>();
+	
+	public string[] prefabNames;
+	
+	void Awake()
+	{
+
+		if (Instance != null)
+		{
+			Destroy(gameObject);
+		}
+		else
+		{
+			Instance = this;
+		}
+		
+	}
+	
+	// Use this for initialization
+	void Start ()
+	{
+		for (int i = 0; i < prefabNames.Length; i++)
+		{
+			if (!pooldict.ContainsKey(prefabNames[i]))
+			{
+				GameObjPool objpool = new GameObjPool();
+				objpool.grawable = Grawable;
+				objpool.maxobj = MaxObj;
+				objpool.prefabName = prefabNames[i];
+				objpool.PreCreate(gameObject);
+				pooldict.Add(prefabNames[i], objpool);
+			}
+		}
+		
+		DontDestroyOnLoad(gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{	
+	}
+	
+	public GameObject GetObjRes(string prefabname)
+	{
+		if (pooldict.ContainsKey(prefabname))
+		{
+			return pooldict[prefabname].GetObject();
+		}
+		else
+		{
+			//Debug.Log("Prefab pool not found! Create New: " + prefabname);
+			
+			GameObjPool objpool = new GameObjPool();
+			objpool.grawable = Grawable;
+			objpool.maxobj = MaxObj;
+			objpool.prefabName = prefabname;
+			objpool.PreCreate(gameObject);
+			pooldict.Add(prefabname, objpool);
+			return pooldict[prefabname].GetObject();
+		}
+		
+		//return null;
+	}
+
+	public GameObject GetObjPrefab(GameObject prefab)
+	{
+		if (pooldict.ContainsKey(prefab.GetHashCode().ToString()))
+		{
+			return pooldict[prefab.GetHashCode().ToString()].GetObject();
+		}
+		else
+		{
+			Debug.Log("Prefab pool not found! Create New: " + prefab.name);
+			
+			GameObjPool objpool = new GameObjPool();
+			objpool.grawable = Grawable;
+			objpool.maxobj = MaxObj;
+			objpool.prefabName = prefab.name;
+			objpool.PreCreatePrefab(prefab, gameObject);
+			pooldict.Add(prefab.GetHashCode().ToString(), objpool);
+			return pooldict[prefab.GetHashCode().ToString()].GetObject();
+		}
+
+	}
+
+	public void RestoreObj(GameObject obj)
+	{
+		obj.SetActive(false);
+	}
+	
+	public void DisableAll()
+	{
+		foreach(KeyValuePair<string, GameObjPool> pa in pooldict)
+		{
+			pa.Value.DisableAll();
+		}
+	}
+
+
+
+}
